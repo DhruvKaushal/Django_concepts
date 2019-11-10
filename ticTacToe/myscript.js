69c7c559b2f78cf28ff7002acb9a12833f4c1859
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-11-10 20:32:24 +0530

    tic tac toe done

--- ticTacToe/myscript.js
+++ ticTacToe/myscript.js
@@ -14,6 +14,17 @@ function clearBoard(){
 restart.addEventListener('click', clearBoard);
 
 //check the square marker
-
+function changeMarker(){
+    if(this.textContent===''){
+        this.textContent = 'X';
+    } else if(this.textContent==='X'){
+        this.textContent = 'O';
+    } else{
+        this.textContent = '';
+    }
+}
 
 //For loop to add event listener to all the squares
+for (var i = 0; i< squares.length; i++) {
+    squares[i].addEventListener('click', changeMarker);
+}
